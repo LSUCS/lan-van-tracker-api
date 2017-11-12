625a9ab502d13d2ca45dad5ccf5123ac28ad47a0
Zack <zackpollard@ymail.com>
2017-11-12 18:25:49 +0000

    Make host IP customisable so it can be run as localhost if required

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
+
 var serverPort = 8080;
+var serverIP = '0.0.0.0';
 
 var cors = require('cors');
 
@@ -58,7 +60,7 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   app.use(middleware.swaggerUi());
 
   // Start the server
-  http.createServer(app).listen(serverPort, function () {
+  http.createServer(app).listen(serverPort, serverIP, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
   });
